64979310cc1612bb346ed1794af5af0ccbc22e3a
ELCOT <ELCOT@Lenovo>
2024-07-26 09:35:13 -0700

    wdwd4

--- Menaka/src/project/Menaka.java
+++ Menaka/src/project/Menaka.java
@@ -3,7 +3,8 @@ package project;
 public class Menaka {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	int Snumber=20;
+	System.out.println("value of the number variable is : "+Snumber);
 
 	}
 
